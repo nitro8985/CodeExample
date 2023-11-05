dff6ca94cb6cf494ae51fc30b5a10ca5de1dc511
nitro <104995604+nitro8985@users.noreply.github.com>
2023-11-05 22:37:27 +0600

    Some fixes

--- src/test/java/SignUpFormApiTest.java
+++ src/test/java/SignUpFormApiTest.java
@@ -12,34 +12,49 @@ import utils.PersonalDataProvider;
 
 public class SignUpFormApiTest {
 
-    private final String textOnSuccess = "Сейчас на ваш телефон поступит звонок, последние 4 цифры являются кодом";
-    private final String messageOnReject = "не может быть пустым";
-    private final String messageOnValidationFault = "must be a valid email";
+    private final String TEXT_ON_SUCCESS = "Сейчас на ваш телефон поступит звонок, последние 4 цифры являются кодом";
+    private final String MESSAGE_ON_REJECT = "не может быть пустым";
+    private final String MESSAGE_ON_VALIDATION_FAULT = "must be a valid email";
 
-    @Test(dataProvider = "fakeData", dataProviderClass = PersonalDataProvider.class, priority = 1)
+    private final String TYPE_PATH = "type";
+    private final String TEXT_PATH = "text";
+    private final String MESSAGE_PATH = "message";
+
+    @Test(description = "Check that backend accepts valid data",
+            dataProvider = "fakeData",
+            dataProviderClass = PersonalDataProvider.class,
+            priority = 1)
     public void backendAcceptsValidDataTest(PersonalData pd) {
         Response response = postData(pd);
-        Assert.assertEquals(response.statusCode(), HttpStatus.SC_OK);
-        Assert.assertTrue(response.body().jsonPath().getBoolean("type"));
-        Assert.assertEquals(response.body().jsonPath().getString("text"), textOnSuccess);
+        Assert.assertEquals(response.statusCode(), HttpStatus.SC_OK, "Status code mismatch");
+        Assert.assertTrue(response.body().jsonPath().getBoolean(TYPE_PATH), "Wrong type");
+        Assert.assertEquals(response.body().jsonPath().getString(TEXT_PATH), TEXT_ON_SUCCESS, "Text in body mismatch");
     }
 
-    @Test(dataProvider = "fakeData", dataProviderClass = PersonalDataProvider.class, priority = 2)
+    @Test(description = "Check there is validation of form fulfillment on front side",
+            dataProvider = "fakeData",
+            dataProviderClass = PersonalDataProvider.class,
+            priority = 2)
     public void backendRejectsUnfilledTest(PersonalData pd) {
         pd.setUserName("");
         Response response = postData(pd);
-        Assert.assertEquals(response.statusCode(), HttpStatus.SC_OK);
-        Assert.assertFalse(response.body().jsonPath().getBoolean("type"));
-        Assert.assertTrue(response.body().jsonPath().getString("message").contains(messageOnReject));
+        Assert.assertEquals(response.statusCode(), HttpStatus.SC_OK, "Status code mismatch");
+        Assert.assertFalse(response.body().jsonPath().getBoolean(TYPE_PATH), "Wrong type");
+        Assert.assertTrue(response.body().jsonPath().getString(MESSAGE_PATH).contains(MESSAGE_ON_REJECT),
+                "Message in body mismatch");
     }
 
-    @Test(dataProvider = "fakeData", dataProviderClass = PersonalDataProvider.class, priority = 3)
+    @Test(description = "Check there is validation of email on front side",
+            dataProvider = "fakeData",
+            dataProviderClass = PersonalDataProvider.class,
+            priority = 3)
     public void backendValidatesEmailTest(PersonalData pd) {
         pd.setLogin(RandomStringUtils.randomAlphabetic(DataUtil.RANDSTRLEN));
         Response response = postData(pd);
-        Assert.assertEquals(response.statusCode(), HttpStatus.SC_OK);
-        Assert.assertFalse(response.body().jsonPath().getBoolean("type"));
-        Assert.assertTrue(response.body().jsonPath().getString("message").contains(messageOnValidationFault));
+        Assert.assertEquals(response.statusCode(), HttpStatus.SC_OK, "Status code mismatch");
+        Assert.assertFalse(response.body().jsonPath().getBoolean(TYPE_PATH), "Wrong type");
+        Assert.assertTrue(response.body().jsonPath().getString(MESSAGE_PATH).contains(MESSAGE_ON_VALIDATION_FAULT),
+                "Message in body mismatch");
     }
 
     private Response postData(PersonalData pd) {
--- src/test/java/SignUpFormUiTest.java
+++ src/test/java/SignUpFormUiTest.java
@@ -17,7 +17,10 @@ import static com.codeborne.selenide.Selenide.$;
 @Slf4j
 public class SignUpFormUiTest extends BaseUiTest {
 
-    @Test(dataProvider = "fakeData", dataProviderClass = PersonalDataProvider.class, priority = 1)
+    @Test(description = "Check the form accepts valid data",
+            dataProvider = "fakeData",
+            dataProviderClass = PersonalDataProvider.class,
+            priority = 1)
     public void formAcceptsValidDataTest(PersonalData pd) {
         SignUpForm form = new SignUpForm();
         fillTheForm(form, pd);
@@ -25,12 +28,15 @@ public class SignUpFormUiTest extends BaseUiTest {
         try {
             Selenide.switchTo().alert().accept();
         } catch (TimeoutException | AlertNotFoundException e) {
-            log.info("No Alert Appeared: ", e);
+            log.error("No Alert Appeared: ", e);
         }
         $(By.id("loginModalAuth")).shouldBe(Condition.visible);
     }
 
-    @Test(dataProvider = "fakeData", dataProviderClass = PersonalDataProvider.class, priority = 2)
+    @Test(description = "Check there is validation of form fulfillment on front side",
+            dataProvider = "fakeData",
+            dataProviderClass = PersonalDataProvider.class,
+            priority = 2)
     public void cantSubmitUnfilledTest(PersonalData pd) {
         SignUpForm form = new SignUpForm();
         pd.setUserName("");
@@ -38,7 +44,10 @@ public class SignUpFormUiTest extends BaseUiTest {
         Assert.assertFalse(form.isSubmitButtonEnabled());
     }
 
-    @Test(dataProvider = "fakeData", dataProviderClass = PersonalDataProvider.class, priority = 3)
+    @Test(description = "Check there is validation of email on front side",
+            dataProvider = "fakeData",
+            dataProviderClass = PersonalDataProvider.class,
+            priority = 3)
     public void formValidatesEmailTest(PersonalData pd) {
         SignUpForm form = new SignUpForm();
         pd.setLogin(RandomStringUtils.randomAlphabetic(DataUtil.RANDSTRLEN));
